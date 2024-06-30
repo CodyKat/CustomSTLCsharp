d15f34b68b7fcdf945485ef9045aa2cdd49c564d
jaemjeon <jaemjeon@DESKTOP-0O2LIM9>
2024-06-30 16:19:05 +0900

    Vector 첫 커밋

---
+++ CsharpPractice/Vector.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomContainers
+{
+    class CustomVector<T> : IEnumerable<T>
+    {
+        private T[] _items;
+        private int _size;
+        private const int DefaultCapacity = 4;
+
+        public CustomVector()
+        {
+            _items = new T[DefaultCapacity];
+        }
+
+        public int Count => _size;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _items[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (_size == _items.Length)
+            {
+                EnsureCapacity(_size + 1);
+            }
+            _items[_size++] = item;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = Array.IndexOf(_items, item, 0, _size);
+            if (index >= 0)
+            {
+                _size--;
+                if (index < _size)
+                {
+                    Array.Copy(_items, index + 1, _items, index, _size - index);
+                }
+                _items[_size] = default(T);
+                return true;
+            }
+            return false;
+        }
+
+        public void Clear()
+        {
+            Array.Clear(_items, 0, _size);
+            _size = 0;
+        }
+
+        public void EnsureCapacity(int min)
+        {
+            if (_items.Length < min)
+            {
+                int newCapacity = _items.Length == 0 ? DefaultCapacity : _items.Length * 2;
+                if (newCapacity < min) newCapacity = min;
+                T[] newItems = new T[newCapacity];
+                Array.Copy(_items, newItems, _size);
+                _items = newItems;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}

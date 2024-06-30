c53e4e3a4ac014be2e11b78031147e45e8ac933c
jaemjeon <jaemjeon@DESKTOP-0O2LIM9>
2024-06-30 15:56:30 +0900

    List 초기 커밋

---
+++ CsharpPractice/List.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+
+namespace CustomContainers
+{
+    public class CustomList<T> : IEnumerable<T>
+    {
+        private T[] _items;
+        private int _size;
+        private const int DefaultCapacity = 4;
+
+        public CustomList()
+        {
+            _items = new T[DefaultCapacity];
+        }
+
+        public int Count => _size;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _items[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (_size == _items.Length)
+            {
+                EnsureCapacity(_size + 1);
+            }
+            _items[_size++] = item;
+        }
+
+        public bool Remove(T item)
+        {
+            int index = Array.IndexOf(_items, item, 0, _size);
+            if (index >= 0)
+            {
+                _size--;
+                if (index < _size)
+                {
+                    Array.Copy(_items, index + 1, _items, index, _size - index);
+                }
+                _items[_size] = default(T);
+                return true;
+            }
+            return false;
+        }
+        
+        public void clear()
+        {
+            Array.Clear(_items, 0, _size);
+            _size = 0;
+        }
+
+        private void EnsureCapacity(int min)
+        {
+            int newCapacity = _items.Length == 0 ? DefaultCapacity : _items.Length * 2;
+            if (newCapacity < min) newCapacity = min;
+            T[] newItems = new T[newCapacity];
+            Array.Copy(_items, newItems, _size);
+            _items = newItems;
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}

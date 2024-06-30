f690fe89fc81054a064a270b1204531f166df306
jaemjeon <jaemjeon@DESKTOP-0O2LIM9>
2024-06-30 15:36:48 +0900

    초기 커밋.

---
+++ CsharpPractice/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace CustomContainers
+{
+    public class CustomList<T> : IEnumerable<T>
+    {
+        private T[] _itmes;
+        private int _size;
+        private const int DefaultCapacity = 4;
+
+        public CustomList()
+        {
+            _itmes = new T[DefaultCapacity];
+        }
+
+        public int Count => _size;
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= _size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                return _itmes[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _size)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _itmes[index] = value;
+            }
+        }
+
+        public void Add(T item)
+        {
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
